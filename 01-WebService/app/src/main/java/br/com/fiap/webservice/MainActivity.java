43660db4517b9a1402cb5b7fa3a5f8d52b0fe539
logonpf <logonpf@fiap.com.br>
2017-08-08 20:52:00 -0300

    WS Resftul - Integração com a tela e parse do JSON

--- 01-WebService/app/src/main/java/br/com/fiap/webservice/MainActivity.java
+++ 01-WebService/app/src/main/java/br/com/fiap/webservice/MainActivity.java
@@ -1,10 +1,16 @@
 package br.com.fiap.webservice;
 
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -14,25 +20,60 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView resposta;
+    private EditText editCodigo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resposta = (TextView) findViewById(R.id.txt_resposta);
+        editCodigo = (EditText) findViewById(R.id.edt_codigo);
     }
 
-    public void buscar(View view){
+    public void buscar(View view) {
         //Instanciar a classe Task
         BuscaTask task = new BuscaTask();
+        //Recupera o código inserido pelo usuário
+        int codigo = Integer.parseInt(editCodigo.getText().toString());
         //Chamar o método execute
-        task.execute(1);
+        task.execute(codigo);
     }
 
-    private class BuscaTask extends AsyncTask<Integer,Void,String>{
+    private class BuscaTask extends AsyncTask<Integer, Void, String> {
+
+        private ProgressDialog progress;
+
+        @Override
+        protected void onPreExecute() {
+            progress = ProgressDialog.show(MainActivity.this,
+                    "Aguarde","Buscando no servidor");
+        }
 
         //Método executado após o método doInBackground
         @Override
         protected void onPostExecute(String s) {
-            Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show();
+            progress.dismiss(); //fecha o dialogo de progresso
+            if (s != null) {
+                //Recuperar os valores do JSON
+                try {
+                    JSONObject json = new JSONObject(s);
+                    int codigo = json.getInt("codigo");
+                    String desc = json.getString("descricao");
+                    int qtd = json.getInt("quantidade");
+                    double preco = json.getDouble("preco");
+
+                    resposta.setText(codigo + " - " + desc +
+                            " - " + preco + " - " + qtd);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Toast.makeText(MainActivity.this,
+                            "Erro", Toast.LENGTH_SHORT).show();
+            }
         }
 
         //Método que executa a tarefa "pesada"
@@ -41,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
             //Chamar o webservice restful
             try {
                 //Criar a URL (localhost é 10.0.2.2)
-                URL url = new URL("http://10.20.63.61:8080/MercadoFiap/rest/mercado/"+params[0]);
+                URL url = new URL("http://10.20.63.61:8080/MercadoFiap/rest/mercado/" + params[0]);
                 //Obter uma conexão
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
@@ -49,10 +90,10 @@ public class MainActivity extends AppCompatActivity {
                 //Método HTTP -> GET (Busca)
                 connection.setRequestMethod("GET");
                 //Tipo de dado que será devolvido pelo webservice (JSON)
-                connection.setRequestProperty("Accept","application/json");
+                connection.setRequestProperty("Accept", "application/json");
 
                 //Status Code HTTP 200 OK ->Sucesso!
-                if (connection.getResponseCode() == 200){
+                if (connection.getResponseCode() == 200) {
                     //Ler a resposta enviada pelo webservice
                     BufferedReader reader = new BufferedReader(
                             new InputStreamReader(connection.getInputStream()));
@@ -60,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
                     StringBuilder json = new StringBuilder();
                     String linha;
                     //Ler todas as linhas retornadas pelo ws
-                    while ((linha = reader.readLine()) != null){
+                    while ((linha = reader.readLine()) != null) {
                         //Adiciona cada linha no builder
                         json.append(linha);
                     }
